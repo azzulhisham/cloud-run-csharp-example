a920569010b0f7b0afc0b14f80e7a81940d5e783
zulhisham <zultan@pinc.my>
2023-01-19 22:08:37 +0800

    initial commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+// Copyright 2022 Google Inc.
+
+var builder = WebApplication.CreateBuilder(args);
+
+var port = Environment.GetEnvironmentVariable("MY_PORT") ?? "8080";
+var url = $"http://0.0.0.0:{port}";
+var target = Environment.GetEnvironmentVariable("TARGET") ?? "World";
+
+var app = builder.Build();
+
+app.MapGet("/", () => $"Hello {target}!");
+
+app.Run(url);
+
+
